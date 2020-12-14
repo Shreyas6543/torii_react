56e16293207625d72188a8b97c46551de40977f3
Shreyas6543 <shreyasm6543@gmail.com>
2020-12-14 16:59:42 +0530

    c

--- app/src/Component/aboutpage/footer.js
+++ app/src/Component/aboutpage/footer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import right from './../../Assets/images/arrow-right.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook , faInstagram , faTwitter } from '@fortawesome/free-brands-svg-icons'
